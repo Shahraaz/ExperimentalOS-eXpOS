80e36e351a56dda3f6cca09d19988ba462337bf8
shahraaz hussain <shahraazhussain@gmail.com>
2019-09-01 01:03:17 +0530

    Add Stage 10 Himanshu

---
+++ batch.bat
@@ -0,0 +1,7 @@
+load --os $HOME/myexpos/spl/spl_progs/os_startup.xsm
+load --init $HOME/myexpos/expl/expl_progs/squares.xsm
+load --exhandler $HOME/myexpos/spl/spl_progs/haltprog.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --int=timer $HOME/myexpos/spl/spl_progs/sampleTimer.xsm
+load --int=7 $HOME/myexpos/spl/spl_progs/int7.xsm
+load --int=10 $HOME/myexpos/spl/spl_progs/haltprog.xsm
